69b7fd3123cda12e2e157ee5274907b0191d9b60
axell-brendow <breno.axel@gmail.com>
2020-02-12 10:59:49 -0300

    Finalize first version for question 01

--- 01_math_problem.cpp
+++ 01_math_problem.cpp
@@ -4,22 +4,64 @@
 
 using namespace std;
 
-int getMaxSumForElement(int x, int y, vector<vector<int>>& matrix, int matrixSize)
+// l = line, c = column
+int sumMatrix(vector<vector<int>>& matrix, int l0, int c0, int l1, int c1)
+{
+    int sum = 0;
+
+    for (size_t i = l0; i <= l1; i++)
+        for (size_t j = c0; j <= c1; j++)
+            sum += matrix[i][j];
+    
+    return sum;
+}
+
+// l = line, c = column
+int getMaxSumForElement(int l, int c, vector<vector<int>>& matrix, int matrixSize)
 {
     int maxSum = INT_MIN;
+    int sum;
 
+    // Expands vertically
+    for (size_t i = l; i < matrixSize; i++)
+    {
+        sum = sumMatrix(matrix, l, c, i, c);
+        maxSum = sum > maxSum ? sum : maxSum;
+    }
+
+    // Expands horizontally.
+    // j = c + 1 skips the 1-length rectangle
+    for (size_t j = c + 1; j < matrixSize; j++)
+    {
+        sum = sumMatrix(matrix, l, c, l, j);
+        maxSum = sum > maxSum ? sum : maxSum;
+    }
+
+    int greatestCoord = l > c ? l : c;
+    int remainingSpace = matrixSize - 1 - greatestCoord;
+
+    // Expands diagonally.
+    // i = 1 skips the 1-length rectangle
+    for (size_t i = 1; i <= remainingSpace; i++)
+    {
+        sum = sumMatrix(matrix, l, c, l + i, c + i);
+        maxSum = sum > maxSum ? sum : maxSum;
+    }
+    
     return maxSum;
 }
 
 int getMaximalSubrectangleSum(vector<vector<int>>& matrix, int matrixSize)
 {
     int maxSum = INT_MIN;
+    int sum;
 
     for (size_t i = 0; i < matrixSize; i++)
     {
         for (size_t j = 0; j < matrixSize; j++)
         {
-            maxSum = getMaxSumForElement(i, j, matrix, matrixSize);
+            sum = getMaxSumForElement(i, j, matrix, matrixSize);
+            maxSum = sum > maxSum ? sum : maxSum;
         }
     }
 
