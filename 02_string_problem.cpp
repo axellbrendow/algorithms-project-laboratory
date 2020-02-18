8097f65215ff5f5c682db0731791457641615adb
axell-brendow <breno.axel@gmail.com>
2020-02-18 12:21:33 -0300

    Continuing question 02

--- 02_string_problem.cpp
+++ 02_string_problem.cpp
@@ -3,12 +3,44 @@
 #include <string.h>
 #include <windows.h>
 
+#define MAX_NUM_LINES 100
+#define MAX_NUM_COLUMNS 51
+
 using namespace std;
+
+int extractWords(char *line, int length)
+{
+    int pos = 0, j = 0;
+
+    while (line[j] == ' ') j++;
+
+    if (j < length)
+    {
+        line[pos++] = line[j++];
+    }
+
+    for (; j < length; j++)
+    {
+        while (line[j] == ' ') j++;
+
+        if (j < length)
+        {
+            line[pos++] = ' ';
+            line[pos++] = line[j];
+        }
+    }
+
+    line[pos] = '\0';
+
+    return pos;
+}
 
 int main()
 {
-    char linhas[100][51];
-    int numLinhas, len;
+    char linhas[MAX_NUM_LINES][MAX_NUM_COLUMNS];
+    char *line;
+    int lineLengths[MAX_NUM_LINES];
+    int numLinhas, len, maxLineLength = 0;
     cin >> numLinhas;
 
     while (numLinhas > 0)
@@ -17,13 +49,17 @@ int main()
 
         for (size_t i = 0; i < numLinhas; i++)
         {
-            fgets(linhas[i], 51, stdin);
-            len = strlen(linhas[i]) - 1;
+            line = linhas[i];
 
-            for (size_t j = 0; j < len; j++)
-            {
-                //
-            }
+            fgets(line, MAX_NUM_COLUMNS, stdin);
+            len = strlen(line) - 1;
+            puts("\n----- BEFORE -------\n\n");
+            puts(line);
+            
+            lineLengths[i] = extractWords(line, len);
+            puts("\n----- AFTER -------\n\n");
+            puts(line);
+            maxLineLength = lineLengths[i] > maxLineLength ? lineLengths[i] : maxLineLength;
         }
         
         cin >> numLinhas;
