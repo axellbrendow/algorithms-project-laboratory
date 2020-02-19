f6f0d40c96332054aabac986d5a18b3bd8cfa4c5
axell-brendow <breno.axel@gmail.com>
2020-02-19 08:11:41 -0300

    Finish question 02_string_problem

--- 02_string_problem.cpp
+++ 02_string_problem.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <stdio.h>
 #include <string.h>
-#include <windows.h>
 
 #define MAX_NUM_LINES 100
 #define MAX_NUM_COLUMNS 51
@@ -12,55 +11,56 @@ int extractWords(char *line, int length)
 {
     int pos = 0, j = 0;
 
-    while (line[j] == ' ') j++;
+    while (j < length && line[j] == ' ') j++;
 
-    if (j < length)
+    while (j < length && line[j] != ' ')
     {
         line[pos++] = line[j++];
     }
 
-    for (; j < length; j++)
+    while (j < length)
     {
-        while (line[j] == ' ') j++;
+        while (j < length && line[j] == ' ') j++;
 
-        if (j < length)
+        if (j < length && line[j] != ' ')
         {
             line[pos++] = ' ';
-            line[pos++] = line[j];
+
+            do line[pos++] = line[j++];
+            while (j < length && line[j] != ' ');
         }
     }
 
     line[pos] = '\0';
 
-    return pos;
+    return pos; // pos is the length of the string at this moment
 }
 
 int main()
 {
-    char linhas[MAX_NUM_LINES][MAX_NUM_COLUMNS];
+    char lines[MAX_NUM_LINES][MAX_NUM_COLUMNS];
     char *line;
-    int lineLengths[MAX_NUM_LINES];
-    int numLinhas, len, maxLineLength = 0;
+    int numLinhas, len, lineLength, maxLineLength;
     cin >> numLinhas;
 
     while (numLinhas > 0)
     {
         scanf("%s");
+        maxLineLength = 0;
 
         for (size_t i = 0; i < numLinhas; i++)
         {
-            line = linhas[i];
+            line = lines[i];
 
             fgets(line, MAX_NUM_COLUMNS, stdin);
             len = strlen(line) - 1;
-            puts("\n----- BEFORE -------\n\n");
-            puts(line);
             
-            lineLengths[i] = extractWords(line, len);
-            puts("\n----- AFTER -------\n\n");
-            puts(line);
-            maxLineLength = lineLengths[i] > maxLineLength ? lineLengths[i] : maxLineLength;
+            lineLength = extractWords(line, len);
+            maxLineLength = lineLength > maxLineLength ? lineLength : maxLineLength;
         }
+
+        for (size_t i = 0; i < numLinhas; i++) printf("%*s\n", maxLineLength, lines[i]);
+        puts("");
         
         cin >> numLinhas;
     }
