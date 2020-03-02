be409b0ddb2bf382689728c77fbc1bd813e8601a
axell-brendow <breno.axel@gmail.com>
2020-03-02 08:34:52 -0300

    Start question 03_town_citizen

---
+++ 03_town_citizen.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int numOfTestCases, numOfCitizens, numOfPairs;
+
+    for (size_t i = 0; i < numOfTestCases; i++)
+    {
+    }
+
+    return 0;
+}
